421d813de2b309ca8ca986bdeacdfe21a2d61fff
Shiyu Wang <shiyu3169@gmail.com>
2019-07-18 13:23:51 -0400

    intro

--- src/component/about/Intro.js
+++ src/component/about/Intro.js
@@ -1,27 +1,28 @@
-import React from "react";
-import about from "../../img/about.jpg";
+import React from 'react';
+import about from '../../img/about.jpg';
 
 const Intro = () => {
-    return (
-        <section id="about-a" className="text-center py-3">
-            <div className="container">
-                <h2 className="section-title">About Me</h2>
-                <div className="bottom-line" />
+  return (
+    <section id='about-a' className='text-center py-3'>
+      <div className='container'>
+        <h2 className='section-title'>About Me</h2>
+        <div className='bottom-line' />
 
-                <div className="about-info">
-                    <img src={about} alt="Qiuting" className="bio-image" />
-                    <div className="bio bg-light">
-                        <h5 className="lead">Shiyu Wang</h5>
-                        <p>
-                            Software developer with 3 years of experience in
-                            designing, developing, debugging and training
-                            students within web application development.
-                        </p>
-                    </div>
-                </div>
-            </div>
-        </section>
-    );
+        <div className='about-info'>
+          <img src={about} alt='Qiuting' className='bio-image' />
+          <div className='bio bg-light'>
+            <h5 className='lead'>Shiyu Wang</h5>
+            <p>
+              I am a full-stack developer with experience in designing,
+              developing, debugging and training students within full-stack
+              development. specializing mostly in JavaScript/Node.js but also
+              work with Java and Python
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Intro;

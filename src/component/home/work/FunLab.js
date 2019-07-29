3935d5b643a9b20907cbe600143315e2a8b512f1
Shiyu Wang <shiyu3169@gmail.com>
2019-07-29 14:52:58 -0400

    add one more fun lab

--- src/component/home/work/FunLab.js
+++ src/component/home/work/FunLab.js
@@ -1,18 +1,24 @@
-import React, { useState } from 'react';
-import Items from '../../utility/Items';
-import item1 from '../../../img/funlab/item1.png';
-import item2 from '../../../img/funlab/item2.png';
+import React, { useState } from "react";
+import Items from "../../utility/Items";
+import item1 from "../../../img/funlab/item1.png";
+import item2 from "../../../img/funlab/item2.png";
+import item3 from "../../../img/funlab/item3.png";
 const FunLab = () => {
   const [items] = useState([
     {
       src: item1,
-      link: 'https://type-n-talk.netlify.com/',
-      code: 'https://github.com/shiyu3169/Type-N-Talk'
+      link: "https://type-n-talk.netlify.com/",
+      code: "https://github.com/shiyu3169/Type-N-Talk"
     },
     {
       src: item2,
-      link: 'https://threejs-animation.netlify.com/',
-      code: 'https://github.com/shiyu3169/Type-N-Talk'
+      link: "https://threejs-animation.netlify.com/",
+      code: "https://github.com/shiyu3169/Type-N-Talk"
+    },
+    {
+      src: item3,
+      link: "https://draggable-and-droppable.netlify.com/",
+      code: "https://github.com/shiyu3169/drag-and-drop"
     }
   ]);
   return <Items items={items} title="Shiyu's Fun Lab" />;

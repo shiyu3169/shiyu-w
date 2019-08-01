dba8e73af9194a1acabbf05f019395aa422c4f51
Shiyu Wang <shiyu3169@gmail.com>
2019-08-01 11:32:55 -0400

    compress png

--- src/component/home/work/FunLab.js
+++ src/component/home/work/FunLab.js
@@ -3,6 +3,7 @@ import Items from "../../utility/Items";
 import item1 from "../../../img/funlab/item1.png";
 import item2 from "../../../img/funlab/item2.png";
 import item3 from "../../../img/funlab/item3.png";
+import item4 from "../../../img/funlab/item4.png";
 const FunLab = () => {
   const [items] = useState([
     {
@@ -19,6 +20,11 @@ const FunLab = () => {
       src: item3,
       link: "https://pixabay-image.netlify.com/",
       code: "https://github.com/shiyu3169/PixaBay_Image_Finder"
+    },
+    {
+      src: item4,
+      link: "https://sliding-sign-in.netlify.com/",
+      code: "https://github.com/shiyu3169/sliding-sign-in-sign-up-form"
     }
   ]);
   return <Items items={items} title="Shiyu's Fun Lab" />;

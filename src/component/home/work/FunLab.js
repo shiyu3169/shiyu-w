84ba5049023519e6535b4071327b3f3f86f6ecc7
Shiyu Wang <shiyu3169@gmail.com>
2019-08-12 11:42:42 -0400

    split screen

--- src/component/home/work/FunLab.js
+++ src/component/home/work/FunLab.js
@@ -1,42 +1,42 @@
-import React, { useState } from "react";
-import Items from "../../utility/Items";
-import item1 from "../../../img/funlab/item1.png";
-import item2 from "../../../img/funlab/item2.png";
-import item3 from "../../../img/funlab/item3.png";
-import item4 from "../../../img/funlab/item4.png";
-import item5 from "../../../img/funlab/item5.png";
-import item6 from "../../../img/funlab/item6.png";
+import React, { useState } from 'react';
+import Items from '../../utility/Items';
+import item1 from '../../../img/funlab/item1.png';
+import item2 from '../../../img/funlab/item2.png';
+import item3 from '../../../img/funlab/item3.png';
+import item4 from '../../../img/funlab/item4.png';
+import item5 from '../../../img/funlab/item5.png';
+import item6 from '../../../img/funlab/item6.png';
 const FunLab = () => {
   const [items] = useState([
     {
       src: item1,
-      link: "https://type-n-talk.netlify.com/",
-      code: "https://github.com/shiyu3169/Type-N-Talk"
+      link: 'https://type-n-talk.netlify.com/',
+      code: 'https://github.com/shiyu3169/Type-N-Talk'
     },
     {
       src: item2,
-      link: "https://threejs-animation.netlify.com/",
-      code: "https://github.com/shiyu3169/Type-N-Talk"
+      link: 'https://split-screen-slider.netlify.com/',
+      code: 'https://github.com/shiyu3169/screen_split_slider'
     },
     {
       src: item3,
-      link: "https://pixabay-image.netlify.com/",
-      code: "https://github.com/shiyu3169/PixaBay_Image_Finder"
+      link: 'https://pixabay-image.netlify.com/',
+      code: 'https://github.com/shiyu3169/PixaBay_Image_Finder'
     },
     {
       src: item4,
-      link: "https://sliding-sign-in.netlify.com/",
-      code: "https://github.com/shiyu3169/sliding-sign-in-sign-up-form"
+      link: 'https://sliding-sign-in.netlify.com/',
+      code: 'https://github.com/shiyu3169/sliding-sign-in-sign-up-form'
     },
     {
       src: item5,
-      link: "https://image-filter.netlify.com/",
-      code: "https://github.com/shiyu3169/online_image_filter"
+      link: 'https://image-filter.netlify.com/',
+      code: 'https://github.com/shiyu3169/online_image_filter'
     },
     {
       src: item6,
-      link: "https://social-accordion.netlify.com/",
-      code: "https://github.com/shiyu3169/social_accordion"
+      link: 'https://social-accordion.netlify.com/',
+      code: 'https://github.com/shiyu3169/social_accordion'
     }
   ]);
   return <Items items={items} title="Shiyu's Fun Lab" />;

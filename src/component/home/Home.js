c023916347814b7d239f0cfed551df0988920dbd
Shiyu Wang <shiyu3169@gmail.com>
2019-09-06 11:44:14 -0400

    make more on github button after each section on work section:

--- src/component/home/Home.js
+++ src/component/home/Home.js
@@ -1,23 +1,43 @@
-import React from 'react';
+import React from "react";
 
-import Header from './Header';
-import Applications from './work/Applications';
-import Design from './work/Design';
-import FunLab from './work/FunLab';
+import Header from "./Header";
+import Applications from "./work/Applications";
+import Design from "./work/Design";
+import FunLab from "./work/FunLab";
 
 const Home = () => {
   return (
     <>
       <Header />
       <Applications />
-      <Design />
-      <FunLab />
-      <div className='py-3 text-center'>
+      <div className="py-3 text-center">
         <a
-          href='https://github.com/shiyu3169?tab=repositories'
-          rel='noopener noreferrer'
-          target='_blank'
-          className='btn-dark'
+          href="https://github.com/shiyu3169?tab=repositories"
+          rel="noopener noreferrer"
+          target="_blank"
+          className="btn-dark"
+        >
+          More on GitHub
+        </a>
+      </div>
+      <Design />
+      <div className="py-3 text-center">
+        <a
+          href="https://github.com/shiyu3169?tab=repositories"
+          rel="noopener noreferrer"
+          target="_blank"
+          className="btn-dark"
+        >
+          More on GitHub
+        </a>
+      </div>
+      <FunLab />
+      <div className="py-3 text-center">
+        <a
+          href="https://github.com/shiyu3169?tab=repositories"
+          rel="noopener noreferrer"
+          target="_blank"
+          className="btn-dark"
         >
           More on GitHub
         </a>

7bc4d174465ab5f7ec6d31b9a72c712db0458594
Shiyu Wang <shiyu3169@gmail.com>
2019-10-25 15:42:34 -0400

    update fun lab projct

--- src/component/work/FunLab.js
+++ src/component/work/FunLab.js
@@ -1,26 +1,26 @@
-import React, { useState } from "react";
-import Items from "../layout/Items";
-import item1 from "../../img/funlab/item1.png";
-import item3 from "../../img/funlab/item3.png";
-import item5 from "../../img/funlab/item5.png";
+import React, { useState } from 'react';
+import Items from '../layout/Items';
+import item1 from '../../img/funlab/item1.png';
+import item2 from '../../img/funlab/item2.png';
+import item5 from '../../img/funlab/item5.png';
 
 const FunLab = () => {
   const [items] = useState([
     {
       src: item1,
-      link: "https://type-n-talk.netlify.com/",
-      code: "https://github.com/shiyu3169/Type-N-Talk"
+      link: 'https://type-n-talk.netlify.com/',
+      code: 'https://github.com/shiyu3169/Type-N-Talk'
     },
     {
-      src: item3,
-      link: "https://pixabay-image.netlify.com/",
-      code: "https://github.com/shiyu3169/PixaBay_Image_Finder"
+      src: item2,
+      link: 'https://pass-generator.netlify.com/',
+      code: 'https://github.com/shiyu3169/password_generator'
     },
 
     {
       src: item5,
-      link: "https://image-filter.netlify.com/",
-      code: "https://github.com/shiyu3169/online_image_filter"
+      link: 'https://image-filter.netlify.com/',
+      code: 'https://github.com/shiyu3169/online_image_filter'
     }
   ]);
   return <Items items={items} title="Shiyu's Fun Lab" />;

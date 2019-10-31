f832e51ef54bfbe09e7bdd09a657596567ac8773
Shiyu Wang <shiyu3169@gmail.com>
2019-10-31 15:01:40 -0400

    animation on items"

--- src/component/layout/Item.js
+++ src/component/layout/Item.js
@@ -1,31 +1,29 @@
-import React from "react";
+import React from 'react';
+import 'aos/dist/aos.css';
+import AOS from 'aos/dist/aos';
 
 const Item = ({ item }) => {
-    return (
-        <div className="item">
-            <div className="item-image">
-                <img src={item.src} alt="" />
-            </div>
-            <div className="item-text">
-                <div className="item-text-wrap">
-                    <a
-                        target="_blank"
-                        rel="noopener noreferrer"
-                        href={item.link}
-                    >
-                        Open Project
-                    </a>
-                    <a
-                        target="_blank"
-                        rel="noopener noreferrer"
-                        href={item.code}
-                    >
-                        View Code
-                    </a>
-                </div>
-            </div>
+  AOS.init({
+    offset: 200,
+    duration: 1000
+  });
+  return (
+    <div className="item" data-aos="flip-left">
+      <div className="item-image">
+        <img src={item.src} alt="" />
+      </div>
+      <div className="item-text">
+        <div className="item-text-wrap">
+          <a target="_blank" rel="noopener noreferrer" href={item.link}>
+            Open Project
+          </a>
+          <a target="_blank" rel="noopener noreferrer" href={item.code}>
+            View Code
+          </a>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Item;

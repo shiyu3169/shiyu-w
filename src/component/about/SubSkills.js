af47843d662cf7ef9d85c2120cf72c4e158df1ed
Shiyu Wang <shiyu3169@gmail.com>
2019-11-04 15:45:11 -0500

    fix subskills grid-gap remove languages add subskills

--- src/component/about/SubSkills.js
+++ src/component/about/SubSkills.js
@@ -1,40 +1,42 @@
-import React, { useState } from "react";
-import Title from "../layout/Title";
-import SubSkill from "./SubSkill";
+import React, { useState } from 'react';
+import Title from '../layout/Title';
+import SubSkill from './SubSkill';
 
 const SubSkills = () => {
   const [skills] = useState([
     {
-      name: "Object Oriented Programming",
-      url: "https://en.wikipedia.org/wiki/Object-oriented_programming"
+      name: 'Object Oriented Programming',
+      url: 'https://en.wikipedia.org/wiki/Object-oriented_programming'
     },
     {
-      name: "Test-driven Development",
-      url: "https://en.wikipedia.org/wiki/Test-driven_development"
+      name: 'Test-driven Development',
+      url: 'https://en.wikipedia.org/wiki/Test-driven_development'
     },
     {
-      name: "Agile Software Development",
-      url: "https://en.wikipedia.org/wiki/Agile_software_development"
+      name: 'Agile Software Development',
+      url: 'https://en.wikipedia.org/wiki/Agile_software_development'
     },
-    { name: "React", url: "https://reactjs.org/" },
-    { name: "Redux", url: "https://redux.js.org/" },
-    { name: "React Native", url: "https://facebook.github.io/react-native/" },
-    { name: "Angular", url: "https://angular.io/" },
-    { name: "Vue", url: "https://vuejs.org/" },
-    { name: "TypeScript", url: "https://www.typescriptlang.org/" },
-    { name: "jQuery", url: "https://jquery.com/" },
-    { name: "Bootstrap", url: "https://getbootstrap.com/" },
-    { name: "Node.js", url: "https://nodejs.org/en/" },
-    { name: "Express", url: "https://expressjs.com/" },
-    { name: "Sass/Scss", url: "https://sass-lang.com/" },
-    { name: "Spring Framework", url: "https://spring.io/" },
-    { name: "Jasmine", url: "https://jasmine.github.io/" },
-    { name: "MongoDB", url: "https://www.mongodb.com/" },
-    { name: "Amazon Web Service", url: "https://aws.amazon.com/" },
-    { name: "MySQL", url: "https://www.mysql.com/" },
-    { name: "JIRA", url: "https://www.atlassian.com/software/jira" },
-    { name: "Git", url: "https://git-scm.com/" },
-    { name: "Heroku", url: "https://heroku.com" }
+    { name: 'React', url: 'https://reactjs.org/' },
+    { name: 'Redux', url: 'https://redux.js.org/' },
+    { name: 'React Native', url: 'https://facebook.github.io/react-native/' },
+    { name: 'Angular', url: 'https://angular.io/' },
+    { name: 'Vue', url: 'https://vuejs.org/' },
+    { name: 'TypeScript', url: 'https://www.typescriptlang.org/' },
+    { name: 'jQuery', url: 'https://jquery.com/' },
+    { name: 'Bootstrap', url: 'https://getbootstrap.com/' },
+    { name: 'Node.js', url: 'https://nodejs.org/en/' },
+    { name: 'Express', url: 'https://expressjs.com/' },
+    { name: 'Sass/Scss', url: 'https://sass-lang.com/' },
+    { name: 'Spring Framework', url: 'https://spring.io/' },
+    { name: 'Jasmine', url: 'https://jasmine.github.io/' },
+    { name: 'MongoDB', url: 'https://www.mongodb.com/' },
+    { name: 'Amazon Web Service', url: 'https://aws.amazon.com/' },
+    { name: 'MySQL', url: 'https://www.mysql.com/' },
+    { name: 'JIRA', url: 'https://www.atlassian.com/software/jira' },
+    { name: 'Git', url: 'https://git-scm.com/' },
+    { name: 'Heroku', url: 'https://heroku.com' },
+    { name: 'Docker', url: 'https://www.docker.com/' },
+    { name: 'Nginx', url: 'https://www.nginx.com/' }
   ]);
   return (
     <div className="container">
--- src/component/page/About.js
+++ src/component/page/About.js
@@ -1,9 +1,9 @@
-import React from "react";
-import InnerHeader from "../layout/InnerHeader";
-import Intro from "../about/Intro";
-import Title from "../layout/Title";
-import Skill from "../about/Skill";
-import SubSkills from "../about/SubSkills";
+import React from 'react';
+import InnerHeader from '../layout/InnerHeader';
+import Intro from '../about/Intro';
+import Title from '../layout/Title';
+import Skill from '../about/Skill';
+import SubSkills from '../about/SubSkills';
 
 const About = () => {
   return (
@@ -13,12 +13,11 @@ const About = () => {
       <section id="about-b" className="bg-dark py-3">
         <div className="container">
           <Title text="Programming Languages" />
-          <Skill title="JavaScript, JSX, TypeScript" width="100%" />
+          <Skill title="JavaScript" width="100%" />
           <Skill title="HTML, CSS" width="100%" />
           <Skill title="SQL, NoSQL" width="95%" />
           <Skill title="Java" width="90%" />
           <Skill title="Python" width="80%" />
-          <Skill title="R" width="50%" />
         </div>
       </section>
       <section id="about-c" className="py-4 bg-light">

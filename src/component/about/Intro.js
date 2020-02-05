72dabeb0da59704f75de3ea41619ef31179340a4
Shiyu Wang <shiyu3169@gmail.com>
2020-02-05 14:46:27 -0500

    update resume

--- src/component/about/Intro.js
+++ src/component/about/Intro.js
@@ -1,23 +1,23 @@
-import React from "react";
-import about from "../../img/about.jpg";
-import resume from "../../files/Shiyu_Wang-Software_Engineer.pdf";
+import React from 'react';
+import about from '../../img/about.jpg';
+import resume from '../../files/Shiyu_Wang_Resume.pdf';
 
 const Intro = () => {
   const intro =
     "I'm a full-stack web developer with hands on experience in designing, developing and maintaining web as well as mobile applications. Possesses diverse experience of creating multiple highly scalable applications using different tech stacks. Adept at understanding the requirements and delivering projects accordingly. Demonstrates the capability of maintaining code by fixing bugs as well as conducting end to end testing. Highly skilled at problem solving to ship mature products within deadlines.";
   return (
-    <section id="about-a" className="text-center py-3">
-      <div className="container">
-        <h2 className="section-title">About Me</h2>
-        <div className="bottom-line" />
+    <section id='about-a' className='text-center py-3'>
+      <div className='container'>
+        <h2 className='section-title'>About Me</h2>
+        <div className='bottom-line' />
 
-        <div className="about-info">
-          <img src={about} alt="Qiuting" className="bio-image" />
-          <div className="bio bg-light">
-            <h1 className="lead">Shiyu Wang</h1>
+        <div className='about-info'>
+          <img src={about} alt='Qiuting' className='bio-image' />
+          <div className='bio bg-light'>
+            <h1 className='lead'>Shiyu Wang</h1>
             <p>{intro}</p>
-            <div className="resume-button">
-              <a href={resume} download className="btn-dark">
+            <div className='resume-button'>
+              <a href={resume} download className='btn-dark'>
                 Download My Latest Resume
               </a>
             </div>
